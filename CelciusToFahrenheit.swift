bd2b1a4cef593d16593bb1e7aa8141886b8bc83f
Nassir ∞ <118899988+soynassir@users.noreply.github.com>
2024-10-31 13:04:59 +0000

    Create CelciusToFahrenheit.swift

---
+++ CelciusToFahrenheit.swift
@@ -0,0 +1,13 @@
+/* MARK: Celsius to Fahrenheit conversion */
+import Cocoa
+
+// Define a constant for temperature in Celsius
+let celsius = 23.4
+
+// Convert Celsius to Fahrenheit
+// Formula: (23.4°C × 9 / 5) + 32 = 74.12°F
+let fahrenheit = (celsius * 9 / 5) + 32
+
+// Print the results
+// Print: Celsius: 23.4°C -> Fahrenheit: 74.12°F
+print("Celcius: \(celsius)°C -> Fahrenheit: \(fahrenheit)°F")
